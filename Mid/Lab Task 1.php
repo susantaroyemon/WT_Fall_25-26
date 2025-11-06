a239e6e36e8706b671d38cfb927318fd9e8959d4
Susanta Roy Emon <115263058+susantaroyemon@users.noreply.github.com>
2025-11-06 10:40:41 +0600

    Add files via upload

---
+++ Mid/Lab Task 1.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <center>
+<h1 style = "color:Blue"> Clinic Patient Registration </h1>
+</center>
+
+</head>
+<body>
+    <center>
+        <table>
+            <tr>
+<td>Full Name: <br></td>
+</tr>
+<tr>
+<td><input type= "text"><br></td>
+    
+</tr>
+
+<tr>
+       
+<td>Age:<br></td>
+</tr>
+<tr>
+<td><input type= "text"><br></td>
+    
+</tr>
+
+<tr>
+        
+<td>Phone Number: <br></td>
+</tr>
+<tr>
+<td><input type= "text"><br></td>
+    
+</tr>
+
+<tr>
+<td>Email Address:<br></td>
+</tr>
+<tr>
+<td><input type= "text"><br></td>
+    
+</tr><tr>
+        
+<td>Insurance Provider: <br></td>
+</tr>
+<tr>
+<td>
+    <select>
+        <option>Select Provider</option></select></td>
+    
+</tr>
+
+<tr>
+        
+<td>Insurance Policy Number:<br></td>
+</tr>
+<tr>
+<td><input type= "text"><br><br></td>
+    
+</tr>
+
+<tr>
+<td>Additional Information <br><br></td>
+    
+</tr>
+
+<tr>
+<td>Username: <br></td>
+</tr>
+<tr>
+<td><input type= "text"><br></td>
+    
+</tr>
+
+<tr>
+       
+<td>Password:<br></td>
+</tr>
+<tr>
+<td><input type= "text"><br></td>
+    
+</tr>
+
+<tr>
+        
+<td>Confirm Password: <br></td>
+</tr>
+<tr>
+<td><input type= "text"><br></td>
+    
+</tr>
+
+  <tr><td><center><input type= "Submit"> </center> </td> </tr>
+
+</center>
+
+
+ </body>
+</html>
